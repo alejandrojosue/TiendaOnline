2591bd81e0c451bd2d829a0303fcf76a5a8f3188
alejandro <josued.ad13@gmail.com>
2023-02-17 11:10:06 -0600

    Inicio de proyecto

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4ab8c63864939a510235a46068cdf0dd'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '161b179a-4242-40c8-b71f-ca01e48a11bb'
+};
