e1dd1514b5d79776a81dc52eff0b18558dcc1b21
alejandro <josued.ad13@gmail.com>
2023-04-06 18:48:05 -0600

    Sockets implementados en el servidor strapi

---
+++ backendStrapi/src/api/product/content-types/product/lifecycles.js
@@ -0,0 +1,22 @@
+module.exports = {
+    beforeCreate(event) {
+        const { data, where, select, populate } = event.params;
+
+        // let's do a 20% discount everytime
+        //   event.params.data.price = event.params.data.price * 0.8;
+
+    },
+
+    afterCreate(event) {
+        const { result, params } = event;
+
+        // do something to the result;
+        strapi.io.emit("products", "create new");
+
+    },
+
+    async afterUpdate(event) {
+        const { result, params } = event;
+        strapi.io.emit("products", "update");
+    }
+};
--- backendStrapi/src/index.js
+++ backendStrapi/src/index.js
@@ -7,7 +7,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register( /*{ strapi }*/) { },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +16,21 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    const io = require('socket.io')(strapi.server.httpServer, {
+      path: '/socket/v1/',
+      cors: { origin: '*', methods: ["GET", "POST", "PUT"] },
+      credential: true,
+    })
+    strapi.io = io
+    io.on('connection', function (socket) {
+      console.log(socket.io);
+      io.emit("test", "test hellow there");
+      console.log("red the code");
+      io.on('bla', data => {
+        // do something with data
+      })
+    })
+
+  },
 };
