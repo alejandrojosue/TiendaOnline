7205903eab183ff4c794306ee3bd4bf8e9d9c066
Alejandro Díaz <67884952+alejandrojosue@users.noreply.github.com>
2023-12-03 12:45:25 -0600

    Update middlewares.js

--- backendStrapi/config/middlewares.js
+++ backendStrapi/config/middlewares.js
@@ -9,4 +9,22 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://tiendamuebles.onrender.com/uploads/'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
